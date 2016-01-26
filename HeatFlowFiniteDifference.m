4d36f7d41aa00997d3b67bb63ec612094abed7b4
JWMize <JWMize>
2016-01-26 16:03:52 -0700

    Attempt part c

--- HeatFlowFiniteDifference.m
+++ HeatFlowFiniteDifference.m
@@ -6,7 +6,7 @@
 
 % Written by JWM for 1/27/2016
 
-clear all;
+clear global;
 figure(1)
 clf
 
@@ -14,19 +14,19 @@ clf
 %This part sets up the values that will be used in the calculations below. 
 
 % Constants
-Ts = -5; % Surface Temperature, C
-Qm = .065; % Heat flow, W/m^2
+Ts = -6.5; % Surface Temperature, C
+Qm = .04; % Heat flow, W/m^2
 k = 2; % Thermal conductivity, W/m*K
-rho = 2700; % density, kg/m^3
+rho = 2000; % density, kg/m^3
 c = 2000; % Specific heat capacity, J/kg*K
 kappa = k/(rho*c); % Thermal diffusivity, m^2/s
 dz = 1; % distance between z steps, m
-DTs = 10; % Step change in Temperature at t=0, C
+DTs = 4; % Step change in Temperature at t=0, C
 
 % Variables and Arrays
 dt = dz^2/(2*kappa)/4; % maximum change in time to retain stability, s
-z = 0:dz:300; % depth array for 300 meters, m
-t = 0:dt:pi*10^7*20; % time array for 100 years, s
+z = 0:dz:400; % depth array for 300 meters, m
+t = 0:dt:pi*10^7*20; % time array for 20 years, s
 T = z*Qm/k+Ts; % Temperature array, C
 T(1) = T(1) + DTs; % Surface Temperature after change, C
 dTdz = zeros(size(T));
@@ -46,6 +46,7 @@ for i = 2:length(t) % Calculates each time step
     
     figure(1)
     plot(T,z,'r','linewidth',4)
+    title(['Temperature profile after ',num2str(t(i)/(pi*10^7)),' years'])
     xlabel('Temperature (C)','fontname','arial','fontsize',21)
     ylabel('Depth (m)','fontname','arial','fontsize',21)
     set(gca,'fontsize',18,'fontname','arial')
@@ -57,17 +58,18 @@ end
 %% Part b
 
 Tinit = z*Qm/k+Ts; % Theoretical geothermal gradient, C
-Tideal = (DTs)*erfc(z/(2*sqrt(kappa*t(length(t)))))+Tinit % Gradient after a step change of DTs, C
+Tideal = (DTs)*erfc(z./(2*sqrt(kappa*t(length(t)))))+Tinit; % Gradient after a step change of DTs, C
 
     figure(1)
     hold on;
     plot(Tideal,z,'b','linewidth',1)
-    xlabel('Temperature (C)','fontname','arial','fontsize',21)
-    ylabel('Depth (m)','fontname','arial','fontsize',21)
-    set(gca,'fontsize',18,'fontname','arial')
-    set(gca,'YDIR','reverse')
-    axis([xaxis_lower xaxis_upper z(1)-10 z(length(z))])
     
 %% Part c
 
-% Haven't figured out part b yet.
+load cape_thompson.dat;
+capedepth = cape_thompson(:,1).';
+capetemp = cape_thompson(:,2).';
+
+figure (1)
+hold on;
+plot (capetemp,capedepth,'o')
